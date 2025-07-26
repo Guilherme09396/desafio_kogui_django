d70e647f1b83e12f45acca867fc6f8195d37a5cf
Guilherme Gomes <guilherme09396@gmail.com>
2025-07-26 11:32:20 -0300

    fix(front): Redirecionando para index da calculadora, caso esteja na pagina de resultado e acione o reload

--- templates/static/calculadora/js/calculator.js
+++ templates/static/calculadora/js/calculator.js
@@ -7,3 +7,10 @@ document.querySelector(".buttons").addEventListener("click", function (event) {
 document.addEventListener('keydown', function (event) {
     checkCommand(event.key);
 })
+
+document.addEventListener("DOMContentLoaded", (ev) => {
+    const navType = performance.getEntriesByType('navigation')[0].type
+    if(navType == 'reload' && ev.target.URL.includes('resultado')) {
+        window.location.href = '/calculadora'
+    }
+})
