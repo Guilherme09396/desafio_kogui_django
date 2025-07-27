10d9cd33c438213fe3d96e4b2636c8b1b5c4f818
Guilherme Gomes <guilherme09396@gmail.com>
2025-07-27 12:43:39 -0300

    feat(front): Adicionando modal para confirmação de deleção do histórico de operações

--- templates/static/calculadora/js/deleteHistory.js
+++ templates/static/calculadora/js/deleteHistory.js
@@ -1,28 +1,40 @@
 import {getCookie} from "./getCookie.js"
 import {showToast} from "../../toast.js"
 
-export function deleteHistory(btnDelete) {
-    if(btnDelete.getAttribute('data-empty')) {
+const confirmDeleteModal = new bootstrap.Modal(document.getElementById('confirmDeleteModal'));
+let deleteCallback = null;
+
+export const deleteHistory = (btnDelete) => {
+    if (btnDelete.getAttribute('data-empty')) {
         showToast("Está vazio, nada a apagar!", 3000, 'error')
         return
     }
+    
+    confirmDeleteModal.show();
+};
 
+document.getElementById('btnConfirmDelete').addEventListener('click', () => {
+    if (deleteCallback) deleteCallback();
+    confirmDeleteModal.hide();
+});
+
+deleteCallback = () => {
     fetch('/calculator/history/delete/', {
-        method: 'DELETE', 
+        method: 'DELETE',
         headers: {
             'X-CSRFToken': getCookie()
         },
     }).then((res) => {
-        if(res.status >= 200 && res.status < 300) {
+        if (res.status >= 200 && res.status < 300) {
             showToast("Histórico apagado!", 500)
             setTimeout(() => {
                 window.location.href = '/calculator'
             }, 800)
         }
 
-        if(res.status == 500) {
+        if (res.status == 500) {
             showToast("Erro interno ao apagar histórico", 3000, 'error')
-        } 
+        }
     }
     ).catch(e => console.log(e))
-}
+};
